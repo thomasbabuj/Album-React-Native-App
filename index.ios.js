c346fee42557d663f9568bd2bf4c50ecaad2c8b4
Thomas Babu <joseph.thomas.babu@gmail.com>
2016-12-03 22:17:53 +0800

    created upto step3

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+// Index.ios.js -- Place code in here for iOS !!
+
+// Step 1 - Import a library to help create a Component
+/*
+   using import destructing, we declare the React variable
+   we will be using in our app.
+   Eg, we are doing this to fix this 'Text' is not defined error
+*/
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+// Step 2 - Create a Component
+// A component is a javascript function
+// returns some amount of JSX
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
+
+// Step 3 - Render it to the device
+/* first parameter -> same name as our project
+ second params -> return all the components avaiable for this app
+ we must register atleast one component
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-export default class albums extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('albums', () => albums);
+AppRegistry.registerComponent('albums', () => App);
