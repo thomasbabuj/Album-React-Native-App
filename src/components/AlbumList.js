d8ebbd86ac0bbabec3d75f97e48b84c209ff13f3
Thomas Babu <joseph.thomas.babu@gmail.com>
2016-12-04 00:03:03 +0800

    refactor Albumlist component from functional to class based component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,27 @@
-import React from 'react';
+/*
+AlbumList is extending Component class. In React we must declare before we use
+any other components. So we must import "Component" from 'react' libaray.
+
+We cant remove React from the import,its manily because babel need this. So here
+when we destructure "Componet" we still import React.
+*/
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List!!!</Text>
-    </View>
-  );
-};
+/*
+ Gonna refactor our current functional code to class component.
+
+ Whenever we define, class component it must defined render method which should
+ return some JSX code.
+*/
+class AlbumList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Album List!!!</Text>
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
