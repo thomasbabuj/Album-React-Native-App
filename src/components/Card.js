03fdf7a0892ebed08e839b8d5c425f2ef80c92f0
Thomas Babu <joseph.thomas.babu@gmail.com>
2016-12-07 17:38:03 +0800

    added styling for card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,8 +4,34 @@ import { View } from 'react-native';
 // Function component
 const Card = () => {
   return (
-    <View />
+    <View style={styles.containerStyle} />
   );
+};
+
+// Adding Styling for Card Component
+const styles = {
+
+  containerStyle: {
+    /* Borders */
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    /* Shadow */
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+
+    elevation: 1,
+
+    /*
+    Margin
+    */
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10
+  }
 };
 
 export default Card;
