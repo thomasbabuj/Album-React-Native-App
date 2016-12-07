b29fab1f3642369a170afbb2071780dc35909674
Thomas Babu <joseph.thomas.babu@gmail.com>
2016-12-07 16:56:50 +0800

    using setState updated the albums on the screen

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -29,18 +29,68 @@ import axios from 'axios';
 */
 class AlbumList extends Component {
 
+  //Step 1 : Set default or Initial state
+  // only for the initialization we declare
+  state = { albums: [] };
+
   componentDidMount() {
      /*
       This will be called when this component about to render on the screen
-      we will be using Axios module to make http request
+      we will be using Axios module to make http request.
+
+      Issue :
+      This request is an async request, so we dont know how long it will take
+      for the response. So during that time, our screen is empty. So Basically we
+      need to rerender our component to update the data.
+
+      Using component level state, we can slove this issue. Using the state, React
+      handles the component changes.
+
+      How state works with React :
+
+        3 Steps :
+          1) Set defaults state
+               Need to add class level property
+          2) Update the state with latest value
+               Using setState() to update the state
+               This is the only way we can update the component state
+          3) Re-render the component state
+
+       Rules of state:
+
+       Definition of state:
+          A plain js object used to record and respond to user-triggered events.
+
+        Whenever we need to update what a component shows, call "this.setState"
+
+        Only change state with setState, do not do like this.state = 123
+
+        Probs is to pass data between paratent to child communication
+        State is to pass data for internel record keeping
      */
+
      axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-          .then(response => console.log(response));
+          .then(response => this.setState({ albums: response.data }));
+  }
+
+  // Helper methods
+  renderAlbums() {
+    // using fat function inside albums map fuction
+    return this.state.albums.map(album => <Text >{album.title}</Text>);
   }
   render() {
+    /*
+    When we console.log here the state, we see the state is logged
+    twice. one is with empty album and the other one is with latest values
+    from the remote url.
+
+          console.log(this.state);
+    */
+
+
     return (
       <View>
-        <Text>Album List!!!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
