9f4f7bbe468de94090c69e766d3bdfe800fb1e6d
Thomas Babu <joseph.thomas.babu@gmail.com>
2016-12-08 10:57:04 +0800

    added images and styles

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
@@ -9,15 +9,53 @@ import CardSection from './CardSection';
  So we can make this as an functional component
 
 props -> receving the props from parent
+destructing props into album
 */
-const AlbumDetail = (props) => (
-  // making use of the album props from parent
-  <Card>
-    <CardSection>
-      <Text>{props.album.title}</Text>
-    </CardSection>    
-  </Card>
-);
+const AlbumDetail = ({ album }) => {
+  // destructing  album into specific variables
+  const { title, artist, thumbnail_image } = album;
+  const { thumbnailStyle, headerContainerStyle } = styles;
 
+  return (
+    // making use of the album props from parent
+    <Card>
+      <CardSection>
+        <View>
+          <Image
+              source={{ uri: thumbnail_image }}
+              style={thumbnailStyle}
+          />
+        </View>
+
+        <View style={headerContainerStyle}>
+          <Text>{ title }</Text>
+          <Text>{ artist }</Text>
+        </View>
+
+      </CardSection>
+    </Card>
+  );
+};
+
+// Styeling
+/*
+   - flexDirection: column ( is the default position of elements )
+   - Text Container
+   - When using Image, we need to manually style the width and height
+     otherwise we wont be able to see the image.
+*/
+const styles = {
+  containerStyle: {
+
+  },
+  headerContainerStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  }
+};
 
 export default AlbumDetail;
